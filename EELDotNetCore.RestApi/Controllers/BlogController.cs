3d5ffdc0ba054d25644ca33e052a378635c6b37e
Eieilin <Eieilin2412@gmail.com>
2024-04-28 19:37:06 +0630

    Coping package from ConsoleApp to RestApi

---
+++ EELDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EELDotNetCore.RestApi.Controllers
+{
+    //https://localhost:3000 => domain url
+    //api/blog => endpoint
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult Read()
+        {
+            return Ok("Read");
+        }
+
+        [HttpPost]
+
+        public IActionResult Create()
+        {
+            return Ok("Create");
+        }
+
+        [HttpPut]
+
+        public IActionResult Update()
+        {
+            return Ok("Update");
+        }
+
+        [HttpPatch]
+
+        public IActionResult Patch()
+        {
+            return Ok("Patch");
+        }
+
+        [HttpDelete]
+
+        public IActionResult Delete()
+        {
+            return Ok("Delete");
+        }
+
+    }
+}
