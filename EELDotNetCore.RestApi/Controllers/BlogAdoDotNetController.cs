7fc708478f02d3f2d1992d5bbd576c5cba9a24b4
Eieilin <Eieilin2412@gmail.com>
2024-05-05 21:04:40 +0630

    WEB API Ado.Net Read and Create Complete.

---
+++ EELDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,90 @@
+﻿using EELDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EELDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from Tbl_Blog";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+          
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            //List<BlogModel> lst = new List<BlogModel>();
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    BlogModel blog = new BlogModel();
+            //    blog.BlogID = Convert.ToInt32(dr["BlogID"]);
+            //    blog.BlogTitle = Convert.ToString(dr["BlogTitle"]);
+            //    blog.BlogAuthor = Convert.ToString(dr["BlogAuthor"]);
+            //    blog.BlogContent = Convert.ToString(dr["BlogContent"]);
+
+            //    BlogModel blogModel = new BlogModel
+            //    {
+            //        BlogID = Convert.ToInt32(dr["BlogID"]),
+            //        BlogTitle = Convert.ToString(dr["BlogTitle"]),
+            //        BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+            //        BlogContent = Convert.ToString(dr["BlogContent"])
+            //    };
+            //    lst.Add(blog);
+            //}
+
+            List<BlogModel> lst = dt.AsEnumerable().Select(dr => new BlogModel
+            {
+                BlogID = Convert.ToInt32(dr["BlogID"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+                BlogContent = Convert.ToString(dr["BlogContent"])
+            }).ToList();
+
+            return Ok(lst);
+
+        }
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+		   ,@BlogAuthor
+		   ,@BlogContent
+		   )";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+          
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Saving successful" : "Saving Fail";
+            //return StatusCode(500, message);
+            return Ok(message);
+
+
+
+        }
+    }
+}
