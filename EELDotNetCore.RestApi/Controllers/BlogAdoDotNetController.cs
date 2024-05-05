d71a7b4a2c482dfb7d3f21a5aeeca7fd6ea8bb6c
Eieilin <Eieilin2412@gmail.com>
2024-05-05 22:21:13 +0630

    RestApi Ado.Net CRUD complete. but not include patch()

--- EELDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
+++ EELDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -1,6 +1,8 @@
 ﻿using EELDotNetCore.RestApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
+//using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -55,6 +57,37 @@ namespace EELDotNetCore.RestApi.Controllers
             return Ok(lst);
 
         }
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            string query = "select * from Tbl_Blog where BlogID = @BlogID";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogID", id);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                return NotFound("No data found");
+            }
+            DataRow dr = dt.Rows[0];
+            var item = new BlogModel
+            {
+                BlogID = Convert.ToInt32(dr["BlogID"]),
+                BlogTitle = Convert.ToString(dr["BlogTitle"]),
+                BlogAuthor = Convert.ToString(dr["BlogAuthor"]),
+                BlogContent = Convert.ToString(dr["BlogContent"])
+            };
+            return Ok(dt);
+        }
+
         [HttpPost]
         public IActionResult CreateBlog(BlogModel blog)
         {
@@ -86,5 +119,50 @@ namespace EELDotNetCore.RestApi.Controllers
 
 
         }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id,BlogModel blog)
+        {
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+        SET [BlogTitle] = @BlogTitle
+            ,[BlogAuthor] = @BlogAuthor
+            ,[BlogContent] = @BlogContent
+        WHERE BlogID = @BlogID
+		   ";
+
+            SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogID", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", blog.BlogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blog.BlogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blog.BlogContent);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Update successful" : "Update Fail";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            string query = @"DELETE FROM [dbo].[Tbl_Blog]
+      WHERE BlogID = @BlogID
+		   ";
+            SqlConnection connection = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+           
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogID", id);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            string message = result > 0 ? "Deleting successful" : "Deleting Fail";
+            return Ok(message); 
+        }
+
     }
 }
