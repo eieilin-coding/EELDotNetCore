d8fbc65804fa08f784bba390301368c07d22b1a5
Eieilin <Eieilin2412@gmail.com>
2024-06-21 23:15:22 +0630

    [Complete] Minimal API CRUD.

---
+++ EELDotNetCore.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,61 @@
+﻿using EELDotNetCore.MinimalApi.Db;
+using Microsoft.EntityFrameworkCore;
+using EELDotNetCore.MinimalApi.Models;
+
+namespace EELDotNetCore.MinimalApi.Features.Blog
+{
+    public static class BlogService
+    {
+        public static IEndpointRouteBuilder MapBlogs(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/Blog", async (AppDbContext db) =>
+            {
+                var lst = await db.Blogs.AsNoTracking().ToListAsync();
+                return Results.Ok(lst);
+            });
+
+            app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
+            {
+                await db.Blogs.AddAsync(blog);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Saving Successful." : "Saving Fail.";
+                return Results.Ok(message);
+            });
+
+            app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogID == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data Found.");
+                }
+
+                item.BlogTitle = blog.BlogTitle;
+                item.BlogAuthor = blog.BlogAuthor;
+                item.BlogContent = blog.BlogContent;
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Updating Successful." : "Updating Fail.";
+                return Results.Ok(message);
+            });
+
+            app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
+            {
+                var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogID == id);
+                if (item is null)
+                {
+                    return Results.NotFound("No data Found.");
+                }
+
+                db.Blogs.Remove(item);
+                var result = await db.SaveChangesAsync();
+
+                string message = result > 0 ? "Deleting Successful." : "Deleting Fail.";
+                return Results.Ok(message);
+            });
+            return app;
+
+        }
+    }
+}
--- EELDotNetCore.MinimalApi/Program.cs
+++ EELDotNetCore.MinimalApi/Program.cs
@@ -1,4 +1,5 @@
 using EELDotNetCore.MinimalApi.Db;
+using EELDotNetCore.MinimalApi.Features.Blog;
 using EELDotNetCore.MinimalApi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,52 +26,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World");
 
-app.MapGet("api/Blog", async (AppDbContext db) =>
-{
-    var lst = await db.Blogs.AsNoTracking().ToListAsync();
-    return Results.Ok(lst);
-});
+//BlogService.MapBlogs(app);
 
-app.MapPost("api/Blog", async (AppDbContext db, BlogModel blog) =>
-{
-    await db.Blogs.AddAsync(blog);
-    var result = await db.SaveChangesAsync();
-
-    string message = result > 0 ? "Saving Successful." : "Saving Fail.";
-    return Results.Ok(message);
-});
-
-app.MapPut("api/Blog/{id}", async (AppDbContext db, int id, BlogModel blog) =>
-{
-    var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogID == id);
-    if (item is null)
-    {
-        return Results.NotFound("No data Found.");
-    }
-
-    item.BlogTitle = blog.BlogTitle;
-    item.BlogAuthor = blog.BlogAuthor;
-    item.BlogContent = blog.BlogContent;
-    var result = await db.SaveChangesAsync();
-
-    string message = result > 0 ? "Updating Successful." : "Updating Fail.";
-    return Results.Ok(message);
-});
-
-app.MapDelete("api/Blog/{id}", async (AppDbContext db, int id) =>
-{
-    var item = await db.Blogs.FirstOrDefaultAsync(x => x.BlogID == id);
-    if (item is null)
-    {
-        return Results.NotFound("No data Found.");
-    }
-
-   db.Blogs.Remove(item);
-    var result = await db.SaveChangesAsync();
-
-    string message = result > 0 ? "Deleting Successful." : "Deleting Fail.";
-    return Results.Ok(message);
-});
+app.MapBlogs();
 
 app.Run();
 
