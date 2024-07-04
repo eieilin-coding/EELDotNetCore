089e3625924cb92b840ed1ebf8e0cea5dc5d73dc
Eieilin <Eieilin2412@gmail.com>
2024-07-04 21:22:55 +0630

    [Complete] Apex Chart -Patterned Donut Pie Chart.

---
+++ EELDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,16 @@
+﻿using EELDotNetCore.MvcChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EELDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PatternedDonutPieChart()
+        {
+            PatternedDonutPieChartModel model = new PatternedDonutPieChartModel();
+            model.Labels = new List<string>() { "Comedy", "Action", "SciFi", "Drama", "Horror" };
+            model.Series = new List<int> { 44, 55, 41, 17, 15 };
+            return View(model);
+        }
+    }
+}
---
+++ EELDotNetCore.MvcChartApp/Models/PatternedDonutPieChart.cs
@@ -0,0 +1,8 @@
+﻿namespace EELDotNetCore.MvcChartApp.Models
+{
+    public class PatternedDonutPieChartModel
+    {
+        public List<int> Series { get; set; }
+       public List<string> Labels { get; set; }
+    }
+}
