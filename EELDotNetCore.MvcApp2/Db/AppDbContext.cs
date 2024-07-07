8f10ad8e5435a9f4e33d0eb78e64fd06647b9803
Eieilin <Eieilin2412@gmail.com>
2024-07-07 20:42:37 +0630

    [Add] MVC app2 structure

---
+++ EELDotNetCore.MvcApp2/Db/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EELDotNetCore.MvcApp2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EELDotNetCore.MvcApp2.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
