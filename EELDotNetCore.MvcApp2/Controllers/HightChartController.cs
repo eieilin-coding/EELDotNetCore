11e2bb67ec0721a2535435a4e0560d1e7d979474
Eieilin <Eieilin2412@gmail.com>
2024-07-13 20:32:31 +0630

    [Complete] Hight Chart.

---
+++ EELDotNetCore.MvcApp2/Controllers/HightChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EELDotNetCore.MvcApp2.Controllers
+{
+    public class HightChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
