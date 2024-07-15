106ce6970af483d160d49b8759a21d8b2633e4af
Eieilin <Eieilin2412@gmail.com>
2024-07-15 20:49:31 +0630

    [Complete] CanvasJs Line Chart.

---
+++ EELDotNetCore.MvcApp2/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EELDotNetCore.MvcApp2.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
